ac984cc3f4c90779bc42c9405f3209bd25e040eb
alvics <alvics71@hotmail.com>
2018-10-05 14:07:38 +1000

    add screenshot abd website link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBkt5o5rwNcKql7bLH4sqYMEQDQPu8ZjFw',
+    authDomain: 'ng-hotel.firebaseapp.com',
+    databaseURL: 'https://ng-hotel.firebaseio.com',
+    projectId: 'ng-hotel',
+    storageBucket: 'ng-hotel.appspot.com',
+    messagingSenderId: '1042201295779'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  { 
+    apiKey: "AIzaSyBkt5o5rwNcKql7bLH4sqYMEQDQPu8ZjFw",
+  authDomain: "ng-hotel.firebaseapp.com",
+  databaseURL: "https://ng-hotel.firebaseio.com",
+  projectId: "ng-hotel",
+  storageBucket: "ng-hotel.appspot.com",
+  messagingSenderId: "1042201295779"
+  }
 };
 
 /*
